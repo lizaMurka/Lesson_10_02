98cd17dbf59045096c34ae75cbba67eaf9beaf73
Sveta <sveta301976@gmail.com>
2024-02-11 09:32:49 +0200

    correction the first task

--- example_2.py
+++ example_2.py
@@ -2,6 +2,8 @@ meters = float(input("Keep the number of meters: "))
 
 choice = input("Keep 1 to convert to miles, 2 for inches, 3 for yards: ")
 
+result = 0
+
 if choice == "1":
   result = meters * 0.000621371
 elif choice == "2":
@@ -10,5 +12,6 @@ elif choice == "3":
   result = meters * 1.09361
 else:
   print("Wrong choice!")
+
 
 print("Result:", result)
